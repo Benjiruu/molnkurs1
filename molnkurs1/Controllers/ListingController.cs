59475e2b9429a2c778646de0aa35304329c6b544
Benjamin Ly <benny_91@hotmail.com>
2024-09-12 10:57:54 +0200

    Add project files.

---
+++ molnkurs1/Controllers/ListingController.cs
@@ -0,0 +1,33 @@
+﻿using ListingService.Data;
+using ListingService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListingController : ControllerBase
+    {
+        private readonly ListingDbContext _context;
+
+        public ListingController(ListingDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/listing
+        [HttpPost]
+        public async Task<IActionResult> PostListing([FromBody] Listing listing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Listings.Add(listing);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostListing", new { id = listing.Id }, listing);
+        }
+    }
+}
